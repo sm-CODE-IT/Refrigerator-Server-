e25d1713fade0f297465b3c718265fa08b97f19e
Sum2n1009 <ksumin4022@naver.com>
2022-08-04 04:44:14 +0900

    join&login (1)

--- src/main/java/com/refrigerator/backend/config/security/JwtTokenProvider.java
+++ src/main/java/com/refrigerator/backend/config/security/JwtTokenProvider.java
@@ -18,6 +18,8 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.List;
 
+// 추가된 라이브러리를 사용해 JWT 생성, 검증 컴포넌트 생성
+// 토큰 만료시간, 회원 권한 정보 저장
 @RequiredArgsConstructor
 @Component
 public class JwtTokenProvider {
@@ -33,6 +35,35 @@ public class JwtTokenProvider {
     //JWT 토큰
     public String createToken(String userPk, List<String> roles) {
         Claims claims = Jwts.claims().setSubject(userPk);
-        return userPk;
+        claims.put("roles", roles);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime()+tokenVaildTime))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getUserPk(token));
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getUserPk(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("X-AUTH-TOKEN");
+    }
+
+    public boolean validateToken(String jwtToken) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
--- src/main/java/com/refrigerator/backend/domain/user/UserRepository.java
+++ src/main/java/com/refrigerator/backend/domain/user/UserRepository.java
@@ -1,7 +1,8 @@
 package com.refrigerator.backend.domain.user;
 
-import com.refrigerator.backend.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
