b05568c4d51f392d59c1b95c0e33c1bd93e74a35
Sum2n1009 <ksumin4022@naver.com>
2022-08-04 01:43:26 +0900

    jsonwebtoken

--- src/main/java/com/refrigerator/backend/config/security/JwtTokenProvider.java
+++ src/main/java/com/refrigerator/backend/config/security/JwtTokenProvider.java
@@ -1,7 +1,38 @@
 package com.refrigerator.backend.config.security;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
 
+@RequiredArgsConstructor
+@Component
 public class JwtTokenProvider {
+    private String secretKey = "webfirwood";
+    private long tokenVaildTime = 30*60*1000L;
+    private final UserDetailsService userDetailsService;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    //JWT 토큰
+    public String createToken(String userPk, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(userPk);
+        return userPk;
+    }
 }
